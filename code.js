25631005ee2c37a5414a7d196d951640892e4693
Christian Black <christianblack@christians-mbp.attlocal.net>
2019-10-08 19:34:53 -0500

    Initial commit with basic geo functionality

---
+++ code.js
@@ -0,0 +1,27 @@
+
+
+let title = document.querySelector('div h1'); 
+console.log(title)
+
+
+
+console.log("hello there")
+
+if ("geolocation" in navigator) {
+    /* geolocation is available */
+    console.log("geo ready")
+} else {
+    /* geolocation IS NOT available */
+    console.log("geo no go")
+}
+
+navigator.geolocation.getCurrentPosition(function (position) {
+
+    let locationName = document.querySelector('#locationName')
+
+    console.log(position.coords.latitude, position.coords.longitude)
+    const latitude = position.coords.latitude; 
+    const longitude = position.coords.longitude; 
+    locationName.innerHTML = '<p>' + latitude + '</p><p>' + longitude + '</p>'; 
+});
+
