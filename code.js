6709f29e9111b4969927cbd0e08a818d1c56f9b0
Christian Black <christianblack@christians-mbp.attlocal.net>
2019-10-14 19:28:18 -0500

    added city search functionality using locationIQ API

--- code.js
+++ code.js
@@ -1,4 +1,3 @@
-<<<<<<< HEAD
 
 
 let title = document.querySelector('div h1'); 
@@ -7,6 +6,8 @@ console.log(title)
 
 
 console.log("hello there")
+
+//geolocation
 
 if ("geolocation" in navigator) {
     /* geolocation is available */
@@ -17,20 +18,55 @@ if ("geolocation" in navigator) {
 }
 
 navigator.geolocation.getCurrentPosition(function (position) {
-
-    let locationName = document.querySelector('#locationName')
+    console.log(position)
 
     console.log(position.coords.latitude, position.coords.longitude)
-    const latitude = position.coords.latitude; 
-    const longitude = position.coords.longitude; 
-    locationName.innerHTML = '<p>' + latitude + '</p><p>' + longitude + '</p>'; 
+    const latitudePosition = position.coords.latitude; 
+    const longitudePosition = position.coords.longitude; 
+    findCity(latitudePosition, longitudePosition); 
+    //set position to innerHTML
+    
+    //TO DO 
+    //use latitude and longitude in maps API to find nearest city
+    //find city function
+    
 });
 
-=======
 document.querySelector("#theTemp").innerHTML = "100";
 
 // TODO: geolocation stuff
 navigator.geolocation.getCurrentPosition(function(position) {
   console.table(position.coords.latitude, position.coords.longitude);
 });
->>>>>>> develop
+
+function findCity(latitudePosition, longitudePosition) {
+    console.log('https://us1.locationiq.com/v1/reverse.php?key=9f7d27c683c49c&lat=' +  latitudePosition + '&lon=' + longitudePosition + '&format=json')
+    let searchURL = 'https://us1.locationiq.com/v1/reverse.php?key=9f7d27c683c49c&lat=' + latitudePosition + '&lon=' + longitudePosition + '&format=json'; 
+    //apiSearch(searchURL)
+    console.log(latitudePosition, longitudePosition)
+    fetch(searchURL)
+        .then(res=> res.json())
+        .then(res => {
+            console.log(res)
+            displayCity(res)
+        })
+        .catch(function (error) {
+            console.log(error)
+        }); 
+    
+}
+// latitudeText.innerHTML = latitude; 
+// longitudeText.innerHTML = longitude; 
+
+//api fetch
+
+// $.ajax(searchURL).done(function (response) {
+//     console.log(response)
+// })
+
+//passes in foundCityName and updates location
+function displayCity(res){
+    cityName = res.display_name; 
+    let location = document.querySelector('#location')
+    location.innerHTML = res.display_name; 
+}
