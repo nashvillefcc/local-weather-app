df2fac09f4d8b7fc128f2102835a6939e4d2ee88
Christian Black <christianblack@Christians-MacBook-Pro.local>
2019-10-16 18:29:14 -0500

    attempt to integrate weather API

--- code.js
+++ code.js
@@ -1,9 +1,24 @@
 
 
 let title = document.querySelector('div h1'); 
-console.log(title)
+console.log(title) 
 
+// function findCity(latitudePosition, longitudePosition) {
+//     console.log('https://us1.locationiq.com/v1/reverse.php?key=9f7d27c683c49c&lat=' + latitudePosition + '&lon=' + longitudePosition + '&format=json')
+//     let searchURL = 'https://api.weather.gov/points/' + latitudePosition + longitudePosition;
+//     //apiSearch(searchURL)
+//     console.log(latitudePosition, longitudePosition)
+//     fetch(searchURL)
+//         .then(res => res.json())
+//         .then(res => {
+//             console.log(res)
+//             displayCity(res)
+//         })
+//         .catch(function (error) {
+//             console.log(error)
+//         });
 
+// }
 
 console.log("hello there")
 
@@ -15,6 +30,7 @@ if ("geolocation" in navigator) {
 } else {
     /* geolocation IS NOT available */
     console.log("geo no go")
+    alert("Unable to display weather without location data.Please refresh to allow location services on your browser")
 }
 
 navigator.geolocation.getCurrentPosition(function (position) {
@@ -23,20 +39,24 @@ navigator.geolocation.getCurrentPosition(function (position) {
     console.log(position.coords.latitude, position.coords.longitude)
     const latitudePosition = position.coords.latitude; 
     const longitudePosition = position.coords.longitude; 
+    
     findCity(latitudePosition, longitudePosition); 
+    findWeather(latitudePosition, longitudePosition)
     //set position to innerHTML
     
     //TO DO 
     //use latitude and longitude in maps API to find nearest city
     //find city function
+    //do something if there is no location enabled
     
 });
 
 document.querySelector("#theTemp").innerHTML = "100";
 
-// TODO: geolocation stuff
+
 navigator.geolocation.getCurrentPosition(function(position) {
   console.table(position.coords.latitude, position.coords.longitude);
+  
 });
 
 function findCity(latitudePosition, longitudePosition) {
@@ -52,8 +72,7 @@ function findCity(latitudePosition, longitudePosition) {
         })
         .catch(function (error) {
             console.log(error)
-        }); 
-    
+        });  
 }
 // latitudeText.innerHTML = latitude; 
 // longitudeText.innerHTML = longitude; 
@@ -70,3 +89,14 @@ function displayCity(res){
     let location = document.querySelector('#location')
     location.innerHTML = res.display_name; 
 }
+
+ 
+
+
+// function findWeather(latitudePosition, longitudePosition) {
+//     fetch(settings)
+//         .then(res => res.json())
+//         .then(res => {console.log(res)
+//         })
+// }
+
